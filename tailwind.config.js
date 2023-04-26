feaeb24f35ef4a4134c4736e74ad2da70674a3a7
Julian Gomez <jgomez109@unab.edu.co>
2023-04-25 19:04:47 -0500

    Install daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: '#146C94',
+
+          secondary: '#444444',
+
+          accent: '#FD841F',
+
+          neutral: '#23282E',
+
+          'base-100': '#0F0E0E',
+
+          info: '#0091D5',
+
+          success: '#B6EB7A',
+
+          warning: '#FFE162',
+
+          error: '#D61355',
+        },
+        light: {
+          primary: '#146C94',
+
+          secondary: '#444444',
+
+          accent: '#FD841F',
+
+          neutral: '#23282E',
+
+          'base-100': '#F3F5F6',
+
+          info: '#0091D5',
+
+          success: '#B6EB7A',
+
+          warning: '#FFE162',
+
+          error: '#D61355',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
 };
