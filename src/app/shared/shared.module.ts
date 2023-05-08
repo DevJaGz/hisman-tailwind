7337132f049be6ada476c3bdad2cdafa53a39d73
Julian Gomez <jgomez109@unab.edu.co>
2023-05-07 19:12:55 -0500

    Add  directive to uppercase

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { ThemeSwitcherComponent } from '@shared/components/theme-switcher/theme-switcher.component';
 import { AlertComponent } from './components/alert/alert.component';
 import { EmptyStateComponent } from './components/empty-state/empty-state.component';
+import { UppercaseInputDirective } from './directives/uppercase-input.directive';
 
 @NgModule({
-	declarations: [ThemeSwitcherComponent, AlertComponent, EmptyStateComponent],
+	declarations: [ThemeSwitcherComponent, AlertComponent, EmptyStateComponent, UppercaseInputDirective],
 	imports: [CommonModule, FormsModule, NgOptimizedImage],
-	exports: [ThemeSwitcherComponent, AlertComponent, EmptyStateComponent, NgOptimizedImage],
+	exports: [ThemeSwitcherComponent, AlertComponent, EmptyStateComponent, NgOptimizedImage, UppercaseInputDirective],
 })
 export class SharedModule {}
