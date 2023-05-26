0832a354c7d519bedc413562fa24b87c1eebacc5
Julian Gomez <jgomez109@unab.edu.co>
2023-05-26 10:28:53 -0500

    Navigate to vehicles from maintenances list

--- src/app/features/maintenances/pages/maintenance-list-page/maintenance-list-page.component.ts
+++ src/app/features/maintenances/pages/maintenance-list-page/maintenance-list-page.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CORE_ROUTE_NAMES } from '@core/core-routing.module';
 import { IVehicle } from '@core/interfaces/vehicle.interface';
 import { MAINTENANCE_ROUTE_NAMES } from '@features/maintenances/maintenances-routing.module';
 import { VEHICLE_BY_LICENSE_RESOLVER_KEY } from '@shared/resolvers/vehicle-by-license.resolver';
@@ -16,6 +17,11 @@ export class MaintenanceListPageComponent implements OnInit {
 	addMaintenance() {
 		const URL = [MAINTENANCE_ROUTE_NAMES.ADD];
 		this.router.navigate(URL, { relativeTo: this.route });
+	}
+
+	seeVehicles() {
+		const URL = [CORE_ROUTE_NAMES.VEHICLES];
+		this.router.navigate(URL);
 	}
 
 	constructor(private route: ActivatedRoute, private router: Router) {}
